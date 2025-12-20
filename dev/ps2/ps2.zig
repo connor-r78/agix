e7f0915ff19251cf1073baf07a39d0f8fe08807a
connor-r78 <connorrakov@gmail.com>
2025-12-20 11:09:58 -0500

    ext2 superblock struct fr

--- dev/ps2/ps2.zig
+++ dev/ps2/ps2.zig
@@ -1,6 +1,23 @@
 //! The PS/2 driver sends a 16-bit integer
 //! 
 //!
+//! Agix Kernel
+//! Copyright (C) 2025 Connor Rakov
+
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const Keyboards = @import("Keyboards.zig");
 
 extern fn inb(port: u16) u8;
 extern fn printc(c: u8) void;
@@ -85,7 +102,7 @@ export fn ps2Controller() u16
 {
   while ( (inb(0x64) & 1) == 0 ) {}
   var char: u16 = convertToASCII(inb(0x60));
-  char = keyboard(char);
+  char = Keyboards.keyboard(char);
   char <<= 8;
   char |= control; 
   return char;
